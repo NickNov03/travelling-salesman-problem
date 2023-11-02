d1d362b8750331ba6d72239cbd4b6239d108ce14
Nick N <nov.nik2020@yandex.ru>
2023-11-02 16:48:34 +0500

    zrt

---
+++ travelling salesman problem/Graph.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace travelling_salesman_problem
+{
+    internal class Graph
+    {
+        double[,] AdjMatrix;
+        int n;
+
+        public Graph(int n)
+        {
+            this.AdjMatrix = new double[n, n];
+            this.n = n;
+        }
+
+        public Graph(double[,] adjMatrix) 
+        {
+            this.AdjMatrix = adjMatrix;
+            this.n = adjMatrix.GetLength(0);
+        }
+
+        public Graph(Graph G)
+        {
+            this.AdjMatrix = G.AdjMatrix;
+            this.n = G.n;
+        }
+
+        private void AddEdge(int v1, int v2, double weight)
+        {
+            AdjMatrix[v1, v2] = weight;
+        }
+
+        private void RemoveEdge(int v1, int v2) 
+        {
+            AdjMatrix[v1, v2] = 0;
+        }
+
+        new public void ToString()
+        {
+            for (int i = 0; i < n; i++) 
+            {
+                Console.Write("{0}: ", i);
+                for (int j = 0; j < n; j++)
+                {
+                    if (!(AdjMatrix[i, j] == 0))
+                        Console.Write("{0} ({1}), ", j, AdjMatrix[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static double[,] GenerateCompleteEuclideanGraph(int n, double MaxEdgeLen)
+        {
+            double[,] matrix = new double[n, n];
+            List<(double, double)> coords = new List<(double, double)>();
+            double x, y;
+            double maxCoord = MaxEdgeLen / Math.Sqrt(2);
+            Random rnd = new Random();
+
+            for (int i = 0; i < n; i++)
+            {
+                x = maxCoord * rnd.NextDouble();
+                y = maxCoord * rnd.NextDouble();
+                coords.Add((x, y));
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    matrix[i, j] = Math.Sqrt((coords[i].Item1 - coords[j].Item1) * (coords[i].Item1 - coords[j].Item1)
+                        + (coords[i].Item2 - coords[j].Item2) * (coords[i].Item2 - coords[j].Item2));
+                    matrix[j, i] = matrix[i, j];
+                }
+            }
+
+            return matrix;
+        }
+
+        private (int, int, double) MinEdge()
+        {
+            double min = double.MaxValue;
+            int min_i = 0, min_j = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    if (AdjMatrix[i,j] < min)
+                    {
+                        min = AdjMatrix[i,j];
+                        min_i = i;
+                        min_j = j;
+                    }
+                }
+            }
+            return (min_i, min_j, min);
+        }
+
+        public (Graph, double) Greedy()
+        {
+            double sum = 0;
+            List<(double, double)> edges = new List<(double, double)>();
+            Graph Copy = new Graph(this);
+            Graph G = new Graph(this.n);
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                (int, int, double) min = Copy.MinEdge();
+                G.AddEdge(min.Item1, min.Item2, min.Item3);
+                //if ()
+            }
+
+            return (G, sum);
+        }
+
+    }
+}
--- travelling salesman problem/Program.cs
+++ travelling salesman problem/Program.cs
@@ -1,9 +1,19 @@
 ﻿
 
+using travelling_salesman_problem;
+
 class Program
 {
     public static void Main()
     {
-        Console.WriteLine("gay");
+        double[,] m = { { 0, 2, 3 }, { 2, 0, 4 }, { 3, 4, 0 }, };
+        Graph G = new Graph(m);
+        G.ToString();
+
+        double[,] m0 = Graph.GenerateCompleteEuclideanGraph(4, 50);
+        Graph H = new Graph(m0);
+        H.ToString();
+
+
     }
 }
